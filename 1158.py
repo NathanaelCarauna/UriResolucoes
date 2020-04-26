37c00d4320d11ebb74ac0bf2bc0c70c2fea15aac
Nathanael <Nathanael.carauna@gmail.com>
2020-04-26 18:12:55 -0300

    submissao do uri 1158

--- 1158.py
+++ 1158.py
@@ -1,9 +1,11 @@
 nEntradas = int(input())
-soma = 0
+
 for i in range(nEntradas):
     x,y = map(int, input().split())
-    for j in range(x,y):
-        if j %2 != 0:
-            soma+=j
+    soma = 0
+    if x%2 == 0:
+            x +=1
+    for n in range(y):        
+        soma += x
+        x+=2
     print(soma)
-
